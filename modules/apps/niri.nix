3a84a95cec5b0e8ae1937fcd443719456b5bf7e0
metamageia <metamageia@gmail.com>
2025-06-08 20:56:07 -0500

    update niri

--- modules/apps/niri.nix
+++ modules/apps/niri.nix
@@ -1,10 +1,17 @@
-{pkgs, config, ...}:
+{ pkgs, config, ... }:
 {
+  services.xserver.displayManager.sddm.enable = true;
+  services.displayManager.sddm.wayland.enable = true;
+  programs.niri.enable = true;
 
-services.xserver.displayManager.sddm.enable = true;
-services.displayManager.sddm.wayland.enable = true;
-programs.niri.enable = true;
+  environment.systemPackages = with pkgs; [
+    qt5.qtwayland
+  ];
 
-environment.sessionVariables.NIXOS_OZONE_WL = "1";
-
+  environment.sessionVariables = {
+    NIXOS_OZONE_WL = "1";
+    QT_QPA_PLATFORM = "wayland";
+    QT_AUTO_SCREEN_SCALE_FACTOR = "1";
+    QT_WAYLAND_DISABLE_WINDOWDECORATION = "1";
+  };
 }
