b15fcab9412afb72a60756e7ca3fa69dd9dcba29
metamageia <metamageia@gmail.com>
2025-06-22 21:41:25 -0500

    Update Obsidian

--- nixos/modules/homeserver.nix
+++ nixos/modules/homeserver.nix
@@ -2,11 +2,23 @@
 {
 
 imports = [
-  ./apps/jellyfin.nix
+  #./apps/jellyfin.nix
 ];
 
 environment.systemPackages = with pkgs; [
-  compose2nix
-  calibre
+  #compose2nix
+  #calibre
+  fluxcd
 ];
+
+  networking.firewall.allowedTCPPorts = [
+    6443 # k3s: required so that pods can reach the API server (running on port 6443 by default)  ];
+  ];
+
+  services.k3s = {
+    enable = true;
+    role = "server";
+  };
+
+
 }
