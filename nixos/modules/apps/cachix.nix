c6cbd0458ae2f5cbc2747db61459f06660e59e64
metamageia <metamageia@gmail.com>
2025-06-25 14:06:51 -0500

    Add Cachix

--- nixos/modules/apps/cachix.nix
+++ nixos/modules/apps/cachix.nix
@@ -1,6 +1,17 @@
+{pkgs, config, ...}:
 {
+  environment.systemPackages = [
+    pkgs.cachix
+  ];
+
   nix.settings = {
-    substituters = [ "https://ezkea.cachix.org" ];
-    trusted-public-keys = [ "ezkea.cachix.org-1:ioBmUbJTZIKsHmWWXPe1FSFbeVe+afhfgqgTSNd34eI=" ];
+    substituters = [
+      "https://cache.nixos.org"
+      "https://nix-community.cachix.org"
+    ];
+
+    trusted-public-keys = [
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    ];
   };
 }
