73cf5d51c256735963b67ba9569a54b543cfb074
metamageia <metamageia@gmail.com>
2025-08-01 12:17:48 -0500

    Change sops key location

--- modules/sops/default.nix
+++ modules/sops/default.nix
@@ -3,10 +3,7 @@
   pkgs,
   inputs,
   ...
-}: let
-  secretsFile = ../../secrets/secrets.yaml;
-  ageKeyFile = "${builtins.getEnv "HOME"}/.config/sops/age/keys.txt";
-in {
+}: {
   imports = [
     inputs.sops-nix.nixosModules.sops
   ];
@@ -18,5 +15,5 @@ in {
   sops.defaultSopsFile = ../../secrets/secrets.yaml;
   sops.defaultSopsFormat = "yaml";
 
-  sops.age.keyFile = "/home/metamageia/.config/sops/age/keys.txt";
+  sops.age.keyFile = "/etc/sops/age/keys.txt";
 }
