b69b95ddefad50acc40a791f98ada9efc192397c
metamageia <metamageia@gmail.com>
2025-08-11 00:56:24 -0500

    Nvidia config

--- hosts/saiadha/configuration.nix
+++ hosts/saiadha/configuration.nix
@@ -31,4 +31,19 @@
     kompose
     kubernetes-helm
   ];
+
+  hardware.graphics = {
+    enable = true;
+  };
+  
+  services.xserver.videoDrivers = ["nvidia"];
+
+  hardware.nvidia = {
+    modesetting.enable = true;
+    powerManagement.enable = false;
+    powerManagement.finegrained = false;
+    open = false;
+    nvidiaSettings = true;
+    package = config.boot.kernelPackages.nvidiaPackages.stable;
+  };
 }
