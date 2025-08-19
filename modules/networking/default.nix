98af673d34537984a401e10c854e2be51d0d6924
metamageia <metamageia@gmail.com>
2025-08-19 16:01:09 -0500

    .

--- modules/networking/default.nix
+++ modules/networking/default.nix
@@ -18,25 +18,14 @@
     };
 
     firewall = {
-      checkReversePath = "loose";
-      trustedInterfaces = ["cni0" "flannel.1"];
-
       allowedTCPPorts = [
-        6443
         2379
         2380
         80
         443
         8096
-        30642
-        32630
-        30231
-        31443
-        30080
-        30443
       ];
       allowedUDPPorts = [
-        8472
         4242
         5353
         7359
