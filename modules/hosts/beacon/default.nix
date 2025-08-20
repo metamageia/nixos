b27cb3a275a27b96f5fe54fe64ec9f54f91b84bb
metamageia <metamageia@gmail.com>
2025-08-19 23:32:39 -0500

    .

--- modules/hosts/beacon/default.nix
+++ modules/hosts/beacon/default.nix
@@ -34,12 +34,4 @@
       size = 2 * 1024;
     }
   ];
-
-  services.caddy = {
-    enable = true;
-    virtualHosts."auriga.gagelara.com".extraConfig = ''
-        reverse_proxy 192.168.100.1:8096 
-      
-    '';
-  };
 }
--- modules/jellyfin/default.nix
+++ modules/jellyfin/default.nix
@@ -20,6 +20,6 @@
   services.jellyfin = {
     enable = true;
     openFirewall = true;
-    user = "metamageia";
+    user = "root";
   };
 }
