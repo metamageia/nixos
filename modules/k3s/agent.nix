9312ab49f06077988050939ec7cdefb603b435b9
metamageia <metamageia@gmail.com>
2025-08-20 01:23:31 -0500

    nodeName on test

--- modules/k3s/agent.nix
+++ modules/k3s/agent.nix
@@ -8,7 +8,7 @@
   services.k3s = {
     role = "agent";
     tokenFile = config.sops.secrets.clusterSecret.path;
-    serverAddr = "https://192.168.100.1:6443";
-    #serverAddr = "https://167.99.123.140:6443";
+    #serverAddr = "https://192.168.100.1:6443";
+    serverAddr = "https://auriga.gagelara.com:6443";
   };
 }
