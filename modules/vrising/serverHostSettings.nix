4cfc0f303f4a14562342248016ae9f3a83c716c6
metamageia <metamageia@gmail.com>
2025-09-03 00:28:32 -0500

    UPDATE

--- modules/vrising/serverHostSettings.nix
+++ modules/vrising/serverHostSettings.nix
@@ -1,47 +1,40 @@
-{
-  pkgs,
-  config,
-  userValues,
-  ...
-}: {
-  sops.secrets = {
-    "passwords/vrising" = {
-      sopsFile = userValues.sopsFile;
+{pkgs, ...}: let
+  serverPassword = "";
+  rconPassword = "";
+
+  serverSettings = {
+    Name = "Crimson Company";
+    Description = "";
+    Port = 9876;
+    QueryPort = 9877;
+    MaxConnectedUsers = 40;
+    MaxConnectedAdmins = 4;
+    ServerFps = 30;
+    SaveName = "crimson-company";
+    Password = serverPassword;
+    Secure = true;
+    ListOnSteam = false;
+    ListOnEOS = false;
+    AutoSaveCount = 20;
+    AutoSaveInterval = 120;
+    CompressSaveFiles = true;
+    GameSettingsPreset = "";
+    GameDifficultyPreset = "";
+    AdminOnlyDebugEvents = true;
+    DisableDebugEvents = false;
+    API = {Enabled = false;};
+    Rcon = {
+      Enabled = false;
+      Port = 25575;
+      Password = rconPassword;
     };
   };
-  systemd.tmpfiles.rules = [
-    "C /vrising/persistentdata/Settings/ServerHostSettings.json - - - - ${config.sops.templates."ServerHostSettings.json".path}"
-  ];
-
-  sops.templates."ServerHostSettings.json".content = ''
-    {
-      "Name": "crimson-company",
-      "Description": "Cracking open some bois with the cold ones",
-      "Port": 9876,
-      "QueryPort": 9877,
-      "MaxConnectedUsers": 40,
-      "MaxConnectedAdmins": 4,
-      "ServerFps": 30,
-      "SaveName": "crimson-company",
-      "Password": "${config.sops.placeholder."passwords/vrising"}",
-      "Secure": true,
-      "ListOnSteam": true,
-      "ListOnEOS": true,
-      "AutoSaveCount": 20,
-      "AutoSaveInterval": 120,
-      "CompressSaveFiles": true,
-      "GameSettingsPreset": "",
-      "GameDifficultyPreset": "",
-      "AdminOnlyDebugEvents": true,
-      "DisableDebugEvents": false,
-      "API": {
-        "Enabled": false
-      },
-      "Rcon": {
-        "Enabled": false,
-        "Port": 25575,
-        "Password": ""
-      }
-    }
-  '';
+in {
+  environment.etc."vrising/persistentdata/Settings/ServerHostSettings.json".source =
+    pkgs.writeText "ServerHostSettings.json"
+      (builtins.toJSON serverSettings);
+  fileSystems."/vrising/persistentdata/Settings/ServerHostSettings.json" = {
+    device = "/etc/vrising/persistentdata/Settings/ServerHostSettings.json";
+    options = [ "bind" ];
+  };
 }
