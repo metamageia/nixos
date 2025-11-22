65cd60d46a674d942b2b2db834646b4910bdad63
metamageia <metamageia@gmail.com>
2025-11-21 18:50:47 -0600

    Update Printing

--- modules/printing/default.nix
+++ modules/printing/default.nix
@@ -6,4 +6,10 @@
 }: {
   services.printing.enable = true;
   services.printing.drivers = with pkgs; [gutenprint];
+
+  # WebUI: http://localhost:631
+
+  environment.systemPackages = [
+    pkgs.kdePackages.okular
+  ];
 }
